6db812bb07781e8a396e382ef294f9b610399943
ayushipatel0603 <77863028+ayushipatel0603@users.noreply.github.com>
2024-12-11 19:19:28 -0500

    Add files via upload

---
+++ Project2/TestStatsLibrary.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class TestStatsLibrary
+{
+    public static void main(String[] args) {
+        StatsLibrary test = new StatsLibrary();
+        System.out.println(test.testall());
+    }
+    
+}
